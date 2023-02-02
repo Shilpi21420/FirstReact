3d339250a68afe5a217b0127e60a25ab36916fea
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-02 14:32:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const App = () =>{
+  return (
+    <>
+      <h1>Expense Items</h1>
+      <p>
+         Food Rs 10 <br />
+         Petrol Rs 100<br/> 
+         Movies Rs 200</p>
+    </>
+  );
+
+}
+export default App;
